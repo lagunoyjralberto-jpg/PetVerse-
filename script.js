591e55c5f710487403df45fb0050b854f7c36d0b
lagunoyjralberto-jpg <lagunoyjralberto@gmail.com>
2026-07-17 11:51:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function startAdventure() {
-    alert("Welcome to PetVerse! Your adventure begins!");
+    document.body.innerHTML = `
+        <h1>Choose Your Hero</h1>
+        <button>⚔️ Warrior</button><br><br>
+        <button>🧙 Mage</button><br><br>
+        <button>🏹 Archer</button>
+    `;
 }
