985013ec7722410b427a594da19d743dc710b29e
lagunoyjralberto-jpg <lagunoyjralberto@gmail.com>
2026-07-17 13:57:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,54 @@ function chooseHero() {
 }
 
 function enterWorld(hero) {
+
+    let hp = 100;
+    let atk = 10;
+
+    if(hero=="Warrior"){
+        hp = 150;
+        atk = 15;
+    }
+
+    if(hero=="Mage"){
+        hp = 80;
+        atk = 25;
+    }
+
+    if(hero=="Assassin"){
+        hp = 90;
+        atk = 20;
+    }
+
+    if(hero=="Archer"){
+        hp = 100;
+        atk = 18;
+    }
+
+    document.body.innerHTML = `
+        <h1>🌍 PetVerse Alpha</h1>
+
+        <h2>${hero}</h2>
+
+        <p>❤️ HP: ${hp}</p>
+
+        <p>⚔ Attack: ${atk}</p>
+
+        <p>⭐ Level: 1</p>
+
+        <p>🐾 Pet: None</p>
+
+        <p>💰 Gold: 0</p>
+
+        <button onclick="alert('Coming Soon!')">🌲 Explore</button>
+
+        <button onclick="alert('Coming Soon!')">🎒 Inventory</button>
+
+        <button onclick="alert('Coming Soon!')">🐾 Pet</button>
+
+        <button onclick="alert('Coming Soon!')">🏰 Guild</button>
+    `;
+}
     document.body.innerHTML = `
         <h1>🌍 PetVerse Alpha</h1>
 
