bcfe74336e6f5cb8086847bb06e125499ed48e88
lagunoyjralberto-jpg <lagunoyjralberto@gmail.com>
2026-07-17 12:19:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function startAdventure() {
     document.body.innerHTML = `
         <h1>Choose Your Hero</h1>
-        <button>⚔️ Warrior</button><br><br>
-        <button>🧙 Mage</button><br><br>
-        <button>🏹 Archer</button>
+
+        <button onclick="enterForest('Warrior')">⚔️ Warrior</button><br><br>
+
+        <button onclick="enterForest('Mage')">🧙 Mage</button><br><br>
+
+        <button onclick="enterForest('Archer')">🏹 Archer</button>
+    `;
+}
+
+function enterForest(hero) {
+    document.body.innerHTML = `
+        <h1>🌲 Whispering Forest</h1>
+
+        <h2>Welcome, ${hero}!</h2>
+
+        <p>Your adventure begins here...</p>
     `;
 }
